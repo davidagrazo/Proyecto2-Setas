b875f99706023e0441a79d31fa277a68464a7689
David Agrazo <d2447237@gmail.com>
2024-02-24 18:43:55 +0100

    cambios

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from inicio import inicio
 from funciones.proyecto2_es import proyecto2es
 from funciones.proyecto2_en import proyecto2en
 
@@ -9,26 +8,19 @@ def main():
         page_title="PROYECTO 2 BOOTCAMP DSB05RT HACKABOSS",
         page_icon=":mushroom:")
 
-    
-    
-    st.sidebar.page_link("https://www.linkedin.com/in/liuva-nu%C3%B1ez-castelo/", label="Liuva Nuñez-Castelo", page_icon=":smile:")
-    st.sidebar.page_link("https://www.linkedin.com/in/alex-garea-4981a6282/", label="Alex Garea")
-    st.sidebar.page_link("https://www.linkedin.com/in/davidagrazods/", label="David Agrazo")
-    
-   
-
     seleccion = st.sidebar.selectbox(
         label="Menu",
-        options=["INFO", "SPANISH VERSION", "ENGLISH VERSION"])
+        options=["SPANISH VERSION", "ENGLISH VERSION"])
 
-    if seleccion == "INFO":
-        inicio()     
-        
     if seleccion == "SPANISH VERSION":
         proyecto2es()
     
     if seleccion == "ENGLISH VERSION":
         proyecto2en()
 
+    st.sidebar.page_link("https://www.linkedin.com/in/liuva-nu%C3%B1ez-castelo/", label="Liuva Nuñez-Castelo")
+    st.sidebar.page_link("https://www.linkedin.com/in/alex-garea-4981a6282/", label="Alex Garea")
+    st.sidebar.page_link("https://www.linkedin.com/in/davidagrazods/", label="David Agrazo")
+    
 if __name__ == "__main__":
     main()
