e9661e76c5510198489e6a7a577a2dffb2ed6ff9
David Agrazo <d2447237@gmail.com>
2024-02-25 11:55:03 +0100

    nueva prueba

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     if seleccion == "ENGLISH VERSION":
         proyecto2en()
 
-    st.sidebar.link_button("Liuva Nuñez-Castelo", "https://www.linkedin.com/in/liuva-nu%C3%B1ez-castelo/")
+    st.sidebar.link_button("Liuva Nuñez-Castelo ", "https://www.linkedin.com/in/liuva-nu%C3%B1ez-castelo/")
     st.sidebar.link_button("Alex Garea", "https://www.linkedin.com/in/alex-garea-4981a6282/")
     st.sidebar.link_button("David Agrazo", "https://www.linkedin.com/in/davidagrazods/")
     
