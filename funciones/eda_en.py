4774fc1ae7aafac05286df6b245296ff43e905c7
David Agrazo <d2447237@gmail.com>
2024-02-27 16:45:17 +0100

    english

--- funciones/eda_en.py
+++ funciones/eda_en.py
@@ -34,12 +34,12 @@ def eda_en():
 
     col1, col2 = st.columns(2)
     
-    fig1 = px.bar(data_frame = df, x= seleccion, color = "class",barmode='group', height=400)
+    fig1 = px.bar(data_frame = df, x= seleccion, color = "CLASS",barmode='group', height=400)
     
     col1.plotly_chart(fig1, use_container_width = True)
     
     fig2 = px.sunburst(data_frame = df,
-        path       = [seleccion, 'class'])
+        path       = [seleccion, 'CLASS'])
     fig2.update_traces(textinfo = "label+percent parent")
     col2.plotly_chart(fig2, use_container_width = True)
 
